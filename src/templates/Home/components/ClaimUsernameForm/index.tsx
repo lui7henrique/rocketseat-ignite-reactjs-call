89508ee80367542e96747ed0acb9f36571e9a3dc
Luiz Henrique <7henrique18@gmail.com>
2023-01-15 13:36:43 -0300

    feat: Using react-hook-form to get input data from ClaimUsernameForm

--- src/templates/Home/components/ClaimUsernameForm/index.tsx
+++ src/templates/Home/components/ClaimUsernameForm/index.tsx
@@ -1,11 +1,32 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
 import * as S from './styles'
 
-export function ClaimUsernameForm() {
+const ClaimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof ClaimUsernameFormSchema>
+
+export const ClaimUsernameForm = () => {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  const handleClaimUsername = useCallback((data: ClaimUsernameFormData) => {
+    console.log({ data })
+  }, [])
+
   return (
-    <S.Form as="form">
-      <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuário" />
+    <S.Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+      <TextInput
+        size="sm"
+        prefix="ignite.com/"
+        placeholder="seu-usuário"
+        {...register('username')}
+      />
 
       <Button size="sm" type="submit">
         Reservar
