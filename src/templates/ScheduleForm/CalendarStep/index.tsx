6d67927b6cecd9d53a54ccb6cc8cfbc7cb78386e
Luiz Henrique <7henrique18@gmail.com>
2023-01-19 13:20:34 -0300

    feat: Visual structure of the timetable

--- src/templates/ScheduleForm/CalendarStep/index.tsx
+++ src/templates/ScheduleForm/CalendarStep/index.tsx
@@ -2,9 +2,33 @@ import { Calendar } from '../../../components/Calendar'
 import * as S from './styles'
 
 export const CalendarStep = () => {
+  const isDateSelected = true
+
   return (
-    <S.Container>
+    <S.Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <S.TimePicker>
+          <S.TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </S.TimePickerHeader>
+
+          <S.TimePickerList>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>09:00h</S.TimePickerItem>
+            <S.TimePickerItem>10:00h</S.TimePickerItem>
+            <S.TimePickerItem>11:00h</S.TimePickerItem>
+            <S.TimePickerItem>12:00h</S.TimePickerItem>
+            <S.TimePickerItem>13:00h</S.TimePickerItem>
+            <S.TimePickerItem>14:00h</S.TimePickerItem>
+            <S.TimePickerItem>15:00h</S.TimePickerItem>
+            <S.TimePickerItem>16:00h</S.TimePickerItem>
+            <S.TimePickerItem>17:00h</S.TimePickerItem>
+            <S.TimePickerItem>18:00h</S.TimePickerItem>
+          </S.TimePickerList>
+        </S.TimePicker>
+      )}
     </S.Container>
   )
 }
