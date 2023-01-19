2183c082d8603d3e4c2b92b96ce65abef3927679
Luiz Henrique <7henrique18@gmail.com>
2023-01-19 13:29:19 -0300

    feat: Confirmation page

---
+++ src/templates/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,45 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import * as S from './styles'
+
+export const ConfirmStep = () => {
+  const handleConfirmScheduling = () => {}
+
+  return (
+    <S.ConfirmForm as="form" onSubmit={handleConfirmScheduling}>
+      <S.FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </S.FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextArea />
+      </label>
+
+      <S.FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </S.FormActions>
+    </S.ConfirmForm>
+  )
+}
