9b32d48ff67e2a0e19a0075c13a3bf76563497c1
Luiz Henrique <7henrique18@gmail.com>
2023-01-23 21:25:56 -0300

    feat: Finishing calculation of days

---
+++ src/components/Calendar/types.ts
@@ -0,0 +1,11 @@
+import dayjs from 'dayjs'
+
+export interface CalendarWeek {
+  week: number
+  days: Array<{
+    date: dayjs.Dayjs
+    disabled: boolean
+  }>
+}
+
+export type CalendarWeeks = CalendarWeek[]
